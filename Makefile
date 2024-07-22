0349d0218621c83943c028a62b960becbdc89ba8
Joao Tiago Ribeiro Pereira <joao-rib@c2r1s6.42porto.com>
2024-07-22 14:30:56 +0100

    2024.07.22 - Functioning program

--- Makefile
+++ Makefile
@@ -1,23 +1,50 @@
 NAME = minishell
-
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror
-SRC = src/main.c src/commands.c src/builtins.c src/utils.c
-OBJ = $(SRC:.c=.o)
-INCLUDE = -Iinclude
+#BONUS_NAME = minishell_bonus
+CC = cc
+FLAGS = -Wall -Wextra -Werror
+LFT_FLAGS = -lft
+RM = rm -fr
+MAIN = minishell.c
+#BONUS = minishell_bonus.c
+SRC = Utils/*.c
+SRC_LFT = libft
+OBJ = obj/*.o
+#BONUS_SRC = libft/*.c Bonus/*.c
+#BONUS_OBJ = obj_bonus/*.o
 
 all: $(NAME)
 
+bonus: $(BONUS_NAME)
+
 $(NAME): $(OBJ)
-	$(CC) $(CFLAGS) $(OBJ) -o $(NAME)
+	@$(CC) $(FLAGS) $(OBJ) -L $(SRC_LFT) $(LFT_FLAGS) -o $(NAME)
+
+#$(BONUS_NAME): $(BONUS_OBJ)
+#	@$(CC) $(FLAGS) $(BONUS_OBJ) -L $(SRC_LFT) $(LFT_FLAGS) -o $(BONUS_NAME)
+
+$(OBJ): $(MAIN) $(SRC)
+	@git clone git@github.com:joao-rib/JP_libft.git libft
+	@make -C $(SRC_LFT) -s
+	@mkdir -p obj
+	@$(CC) $(FLAGS) -c $(SRC) $(MAIN)
+	@mv *.o obj/
+
+#$(BONUS_OBJ): $(BONUS) $(BONUS_SRC)
+#	@git clone git@github.com:joao-rib/JP_libft.git libft
+#	@make -C $(SRC_LFT) -s
+#	@mkdir -p obj_bonus
+#	@$(CC) $(FLAGS) -c $(BONUS_SRC) $(BONUS)
+#	@mv *.o obj_bonus/
 
 clean:
-	rm -f $(OBJ)
+	@$(RM) $(OBJ) $(BONUS_OBJ) obj obj_bonus
+	@make clean -C $(SRC_LFT) -s
 
-fclean: clean
-	rm -f $(NAME)
+fclean:
+	@$(RM) $(OBJ) $(BONUS_OBJ) $(NAME) $(BONUS_NAME) obj obj_bonus
+	@make fclean -C $(SRC_LFT) -s
+	@$(RM) libft
 
 re: fclean all
 
-.PHONY: all clean fclean re
-.silent: Joao was here
+bonus_re: fclean bonus
--- include/minishell.h
+++ include/minishell.h
@@ -3,19 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alfreire <alfreire@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 09:45:30 by alfreire          #+#    #+#             */
-/*   Updated: 2024/07/16 09:46:36 by alfreire         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:31:33 by joao-rib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+//# include <readline/readline.h>
+//# include <readline/history.h>
+# include "../libft/libft.h"
+
+int		error_msg(char *msg);
+
+/*typedef struct s_point
+{
+	int	x;
+	int	y;
+}		t_point;
+
+typedef struct s_map
+{
+	char			**layout;
+	int				num_coins;
+	struct s_point	map_size;
+	struct s_point	pos_exit;
+	struct s_point	pos_player;
+}			t_map;
+
+typedef struct s_sprite
+{
+	int		width;
+	int		height;
+	void	*img;
+}			t_sprite;
+
+typedef struct s_graph
+{
+	void	*mlx;
+	void	*win;
+}			t_graph;
+
+typedef struct s_game
+{
+	struct s_map	*map;
+	struct s_sprite	*sprite;
+	struct s_graph	display;
+	struct s_point	curr;
+	struct s_point	next;
+	int				moves;
+	int				coins;
+	int				nsprites;
+	int				textpos_y;
+}			t_game;
+
+//Utils - Error Handling
+int		error_msg(char *msg);
+void	error_exit(char *msg, t_game *g);
+void	error_map(char *msg, t_game *g);
+//Utils - Loading
+void	load_map(t_game *g, char *file);
+void	load_graphics(t_game *g);
+void	load_sprites(t_game *g, int nsprites);
+//Utils - Rendering
+void	render_map(t_game *g);
+int		render_movement(t_game *g);
+//Utils - Moving
+int		move_handler(int keypress, t_game *g);
+void	move_player(t_game *g);
+//Utils - Destroying
+void	destroy_map(t_game *g);
+int		destroy_game(t_game *game);
+//Utils - Other
+void	validate_map(t_game *g);*/
 
 #endif
