c518b2b0a22a4e46fcc6d75f07af38c0b543abb0
joao-rib <jonnasn@gmail.com>
2024-10-02 00:29:48 +0100

    2024.10.01 - Created sanitizing.c

---
+++ Utils/sanitizing.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reading.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 19:30:33 by joao-rib          #+#    #+#             */
+/*   Updated: 2024/09/25 19:18:01 by joao-rib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+/*void	ft_free(void *p)
+{
+	if (p)
+		free(p);
+	p = NULL;
+}
+
+void	update_envs(void)
+{
+	char	*tmp;
+
+	matrix_destroy(ms()->path);
+	matrix_destroy(ms()->envp);
+	tmp = get_env("PATH");
+	(ms()->path) = ft_split(tmp, ':');
+	(ms()->envp) = envlist_to_matrix(ms()->envlist);
+	free(tmp);
+}
+
+void	sanitize(bool end)
+{
+	ft_free(ms()->input);
+	ft_free(ms()->prompt);
+	ast_clear(ms()->ast, ast_destroy_node);
+	matrix_destroy(ms()->pipes);
+	ft_lstclear(&ms()->lexemes, (void (*)(void *))token_destroy);
+	(ms()->ast) = NULL;
+	(ms()->pipes) = NULL;
+	(ms()->prompt) = NULL;
+	(ms()->lexemes) = NULL;
+	(ms()->num_commands) = 0;
+	if (end)
+	{
+		ft_free(ms()->cwd);
+		matrix_destroy(ms()->path);
+		matrix_destroy(ms()->envp);
+		ft_lstclear(&ms()->envlist, (void (*)(void *))env_destroy);
+		ft_lstclear(&ms()->envtmp, (void (*)(void *))env_destroy);
+		exit(ms()->exit_status);
+	}
+}
+*/
+
+void	sanitize(t_minish *ms)
+{
+
+}
