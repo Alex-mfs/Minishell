c4367f27bb101fce84952243c0b6ac3c29b76b34
Joao Tiago Ribeiro Pereira <joao-rib@c2r4s4.42porto.com>
2024-10-24 11:58:39 +0100

    2024.10.24 - Cleanup and various tests

--- Parse/expanding.c
+++ Parse/expanding.c
@@ -6,7 +6,7 @@
 /*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 19:30:33 by joao-rib          #+#    #+#             */
-/*   Updated: 2024/10/24 11:41:12 by joao-rib         ###   ########.fr       */
+/*   Updated: 2024/10/24 11:58:23 by joao-rib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	expand(t_minish *ms)
 	curr = ms->tk_list;
 	while (curr)
 	{
-		if (curr->type == DOUBLE_QUOTES || curr->type == OTHER) //Pode dar erro em redirects. Testar.
+		if (curr->type == DOUBLE_QUOTES || curr->type == OTHER) //Pode dar erro em redirects, ou poderei ter que dar skip a heredocs. Testar.
 			expand_token(ms, curr);
 		curr = curr->next;
 	}
