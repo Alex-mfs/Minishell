7642805e02e850989e6b6d155a123ae06efa4510
Joao Tiago Ribeiro Pereira <joao-rib@c2r4s4.42porto.com>
2024-10-24 11:52:01 +0100

    2024.10.24 - Cleanup and various tests

--- Utils/reading.c
+++ Utils/reading.c
@@ -6,7 +6,7 @@
 /*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 19:30:33 by joao-rib          #+#    #+#             */
-/*   Updated: 2024/10/24 11:34:06 by joao-rib         ###   ########.fr       */
+/*   Updated: 2024/10/24 11:51:40 by joao-rib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ static void	compute(t_minish *ms, char *input)
 		buff = buff->next;
 		i++;
 	}
+	printf("\n");
 	//END TESTE TOKEN
 	if (!validate_tokens(ms)) //WIP na verdade, é permitido terminar num pipe. Corrigir.
 		return ;
@@ -78,6 +79,7 @@ static void	compute(t_minish *ms, char *input)
 		buff2 = buff2->next;
 		i++;
 	}
+	printf("\n");
 	//END TESTE CMD
 	if(!assign_var(ms)) //WIP Se houver variável para atribuir valor (ex.: BUFFER_SIZE=40), faz-se. Senão, apenas executar (espera, porque senão?)
 		execute(ms);
