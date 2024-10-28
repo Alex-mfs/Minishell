bc235ddd69453fc07fa496265b3bf8452062e0c0
Joao Tiago Ribeiro Pereira <joao-rib@c2r6s8.42porto.com>
2024-10-28 19:31:47 +0000

    2024.10.28 - Header cleanup

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: joao-rib <joao-rib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 09:45:30 by alfreire          #+#    #+#             */
-/*   Updated: 2024/10/28 19:04:06 by joao-rib         ###   ########.fr       */
+/*   Updated: 2024/10/28 19:30:21 by joao-rib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,54 +71,57 @@ typedef struct s_minish
 
 //Utils - Initialising
 void	init_ms(t_minish *ms, char **envp);
+void	set_exit_status(int status); //minishell.c
+int		get_exit_status(void); //minishell.c
+//Utils - Reading
+void	read_inputs(t_minish *ms);
+//Utils - Validating
+bool	validate_quotes(char *input);
+bool	validate_tokens(t_minish *ms);
 //Utils - Signaling
 void	set_signals(void);
 void	set_signals_heredoc(void);
 void	treat_child_signal(void);
-
 //Utils - Handling
 void	handle_interrupt(int signum);
 void	handle_child_interrupt(int signal);
 void	handle_child_quit(int signal);
 void	handle_heredoc_interrupt(int signum);
-
-//Utils - Validating
-bool	validate_quotes(char *input);
-bool	validate_tokens(t_minish *ms);
-//Utils - Reading
-void	read_inputs(t_minish *ms);
-//Utils - Tokening
-void	get_tokens(t_minish *ms, char *input);
-//Utils - Expanding
-void	expand(t_minish *ms);
-//Utils - Parsing
-void	parse(t_minish *ms);
-//Utils - Executing
-void	execute(t_minish *ms);
 //Utils - Sanitizing
 void	sanitize_ms(t_minish *ms, bool sair);
 void	sanitize_envp(t_minish *ms);
 
-//executing_aux
+//Parse
+void	get_tokens(t_minish *ms, char *input);
+void	expand(t_minish *ms);
+void	parse(t_minish *ms);
+//Parse - Listing
+int		cmdlst_size(t_ast *lst, bool total);
+t_ast	*cmdlst_last(t_ast *lst);
+void	cmdlst_addback(t_ast **lst, t_ast *new);
+int		lastpipe_index(t_ast *lst);
+void	tklst_addback(t_token **lst, t_token *new);
+
+//Execution - Executing
+void	execute(t_minish *ms);
+//Execution - Executing_aux
 void	error(char *str, int status);
 bool	is_redir_or_pipe(char *cmd);
 bool	is_redirection(char *cmd);
 bool	need2be_parent(char *command, char *arg);
-
-//pipeline
+//Execution - Pipeline
 void	pipeline_matrix(t_minish *ms);
 void	close_in_out(int index, t_minish *ms);
 void	relinking_in_out(t_minish *ms);
 //void	pipe_data_flow(int cmd_index, t_minish *ms);
 void	pipe_data_flow(int cmd_index, t_minish *ms, char **fullcmd);
 void	exec_if_exists(char **arg, t_minish *ms);
+//Execution - Path
+char	*get_executable_path(char *cmd, t_minish *ms);
+//Execution - Redirection
+void	execute_redir(const char *type, char *filename, t_minish *ms);
 
-//minishell.c - exit_status
-void	set_exit_status(int status);
-int		get_exit_status(void);
-
-
-//commands
+//Commands
 void	exit_bash(char **exit_args, t_minish *ms);
 void	ft_export(char **exp_args, t_minish *ms);
 void	env(char **env_arg, char **env_list);
@@ -126,8 +129,7 @@ char	*get_env(const char *key, char **env_list);
 void	echo(char **words);
 void	unset(char **vars, t_minish *ms);
 void	cd(char **tokens, t_minish *ms);
-
-//export_aux
+//Commands - Export_aux
 int		ft_strlen_sep(const char *s, char *seps);
 char	*extract_key(const char *assignment);
 void	copy_env_except_key(char **src, char **dest, const char *key, int len);
@@ -136,20 +138,6 @@ int		find_env_in_tmp(t_minish *ms, const char *name);
 int		find_env_index(char **target_env, const char *assigment);
 void	add_new_env(char ***target_env, const char *assignment);
 void	add_or_update_env(char ***target_env, const char *assignment);
-
-//path
-char	*get_executable_path(char *cmd, t_minish *ms);
-
-//Redirection
-void	execute_redir(const char *type, char *filename, t_minish *ms);
-
-//Utils(?) - cmd_listing
-int		cmdlst_size(t_ast *lst, bool total);
-t_ast	*cmdlst_last(t_ast *lst);
-void	cmdlst_addback(t_ast **lst, t_ast *new);
-int		lastpipe_index(t_ast *lst);
-//Utils(?) - tk_listing
-void	tklst_addback(t_token **lst, t_token *new);
 
 #endif
 
